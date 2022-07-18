6739343617349df6485e7b30459e5f2f9491ba17
Nhật Ngô Sách <nhat.ns@teko.vn>
2022-07-18 14:48:41 +0700

    minor fix

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -x
 
+DIST_DIR=.dist
+
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
 for chart in ./*; do
     if [ -f "$chart/Chart.yaml" ]; then
-        helm package $chart
+        helm dependency update $chart
+        helm dependency build $chart
+        helm package $chart -d $DIST_DIR
     fi
 done
 
-helm repo index --url https://nhatminhk63j.github.io/charts .
+helm repo index --url https://duyet.github.io/charts $DIST_DIR
+cp README.md $DIST_DIR/
